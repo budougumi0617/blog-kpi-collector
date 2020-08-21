afde0c9fa345d5fd253bea0263dd1a8954dbc8e9
budougumi0617 <budougumi0617@binc.jp>
2020-08-22 00:02:43 +0900

    :heart: fix counting the twitter follower

--- src/twitter.ts
+++ src/twitter.ts
@@ -2,7 +2,7 @@ import URLFetchRequestOptions = GoogleAppsScript.URL_Fetch.URLFetchRequestOption
 
 // https://yurilog.cc/3375
 function getTwitterFollowers(accoutName: string): number {
-  const url: string = `https://twitter.com/${accoutName}`;
+  const url: string = `https://mobile.twitter.com/${accoutName}`;
   const postheader: Object = {
     timeout: "50000",
   };
@@ -12,22 +12,13 @@ function getTwitterFollowers(accoutName: string): number {
     muteHttpExceptions: true,
   };
   let html: string = UrlFetchApp.fetch(url, parameters).getContentText("UTF-8");
-  const searchTag = "followers";
-  let index: number = html.indexOf(searchTag);
-  Logger.log("by Logger %s", index);
-
-  let result: number = -1;
-  if (index !== -1) {
-    html = html.substring(index + searchTag.length);
-    const titleKey = 'title="';
-    index = html.indexOf(titleKey);
-    if (index !== -1) {
-      html = html.substring(index + titleKey.length);
-      index = html.indexOf(' Followers"');
-      if (index !== -1) {
-        result = Number(html.substring(0, index));
-      }
-    }
+  const counts = html.match(/\/followers\">[<>\w\s=/"]*statnum\">(\d+)<\/div>/);
+  if (counts === null) {
+    Logger.log("failed parse twitter");
+    return -1;
+  } else if (counts.length < 2) {
+    Logger.log("failed parse twitter %s", counts);
+    return -1;
   }
-  return result;
+  return Number(counts[1]);
 }
