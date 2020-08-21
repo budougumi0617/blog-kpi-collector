d82b330a5b06da6f6efbad42904b6fb8e0b97dbf
budougumi0617 <budougumi0617@binc.jp>
2020-08-22 00:12:18 +0900

    :memo: fix comments

--- src/twitter.ts
+++ src/twitter.ts
@@ -1,6 +1,5 @@
 import URLFetchRequestOptions = GoogleAppsScript.URL_Fetch.URLFetchRequestOptions;
 
-// https://yurilog.cc/3375
 function getTwitterFollowers(accoutName: string): number {
   const url: string = `https://mobile.twitter.com/${accoutName}`;
   const postheader: Object = {
@@ -12,6 +11,7 @@ function getTwitterFollowers(accoutName: string): number {
     muteHttpExceptions: true,
   };
   let html: string = UrlFetchApp.fetch(url, parameters).getContentText("UTF-8");
+  // curlで取得したHTMLの中から文字列を取得。その文字列をChrome Dev toolsでこねこねして考えた正規表現。
   const counts = html.match(/\/followers\">[<>\w\s=/"]*statnum\">(\d+)<\/div>/);
   if (counts === null) {
     Logger.log("failed parse twitter");
